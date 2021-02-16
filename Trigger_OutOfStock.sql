3ae4f40f394f55875efd203fe8986d392e830ea4
SaraCarlsson83 <71943956+SaraCarlsson83@users.noreply.github.com>
2021-02-16 14:50:14 +0100

    Add files via upload

--- Trigger_OutOfStock.sql
+++ Trigger_OutOfStock.sql
@@ -4,17 +4,19 @@ date datetime not null DEFAULT CURRENT_TIMESTAMP,
 Shoe_id int not null, 
 foreign key (shoe_id) references shoe(id));
 
--- drop trigger last_shoe;
+drop trigger last_shoe;
+
 Delimiter //
 create trigger last_shoe
 after update
 on shoe
 for each row
 Begin
-	if shoe_stock = 0
+	if new.shoe_stock = 0
 		then 
-			insert into out_of_stock (id) values (old.id);
+			insert into out_of_stock (shoe_id) values (old.id);
 	end if;
 End//
 Delimiter ;
  
+ select * from out_of_stock;
