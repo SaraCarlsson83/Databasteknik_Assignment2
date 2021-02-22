0c2ecfd55c4a07465b6f0e6c7e667bb477e5d68e
Ivona Zoricic <72072576+I-Zor@users.noreply.github.com>
2021-02-22 17:13:22 +0100

    Add files via upload

--- Questions2.sql
+++ Questions2.sql
@@ -1,5 +1,5 @@
-select * from rating;
-select * from rating_alternatives;
+-- select * from rating;
+-- select * from rating_alternatives;
 delimiter //
 -- drop function average_rate;
 create function average_rate (shoeId int)
@@ -15,30 +15,31 @@ return average;
 end//
 delimiter ;
 
-select average_rate(40);
+select average_rate(40) as avg;
 
 delimiter //
 create function getTextRating (numberRating double)
 returns varchar(50)
 reads sql data
 begin
+declare answer varchar(50);
 if numberRating > 0 and numberRating <= 1.5
-then return 'Missnöjd';
+then select 'Missnöjd' into answer;
 elseif numberRating > 1.5 and numberRating <= 2.5
-then return 'Ganska nöjd';
+then select 'Ganska nöjd'into answer;
 elseif numberRating > 2.5 and numberRating <= 3.5
-then return 'Nöjd';
+then select 'Nöjd' into answer;
 elseif numberRating > 3.5 and numberRating <= 4
-then return 'Mycket nöjd';
+then select 'Mycket nöjd' into answer;
 end if;
+return answer;
 end//
 
 delimiter ;
 
 select getTextRating(1.6);
 
-
-drop view all_averageRates;
+-- drop view all_averageRates;
 create view all_averageRates as 
 select label.label_name, name.shoe_name, size.size_name, avg(rating_alternatives.rating_numbers), getTextRating(avg(rating_alternatives.rating_numbers)) from shoe
 left join label on label.id = shoe.label_id
@@ -49,7 +50,7 @@ left join rating_alternatives on rating_alternatives.id = rating.RatingAlternati
 group by shoe.id;
 
 select * from all_averageRates;
-select*from shoe;
+-- select*from shoe;
 
 -- Stored procedure "Rate" som lägger till ett betyg och en kommentar på en specifik produkt för en specifik kund
 delimiter //
@@ -63,5 +64,60 @@ delimiter ;
 call Rate (3, null, 45, 12);
 select * from rating;
 
+select * from orders;
+
+delimiter //
+CREATE PROCEDURE `Rate_Strings`(procedure_rate varchar(50), procedure_comment varchar(150), procedure_shoeId int, procedure_customerId int)
+BEGIN
+declare rateId int default 0;
+select rating_alternatives.id from rating_alternatives where rating_alternatives.options = procedure_rate into rateId; 
+insert into rating (RatingAlternatives_id, Comment, Shoe_id, Customer_id) values (rateId, procedure_comment, procedure_shoeId, procedure_customerId);
+END//
+delimiter ;
+
+call Rate_Strings('Mycket nöjd', null, 45, 13);
+
+set sql_safe_updates = 0;
+-- drop procedure AddToCart;
+delimiter //
+CREATE PROCEDURE `AddToCart`(idCustomer int, idShoes int, idOrders int)
+BEGIN
+	declare orderTemp int default 0;
+	declare stockTemp int default 0;
+	select orders.id into orderTemp from Orders where id = idOrders;
+    select shoe.shoe_stock into stockTemp from Shoe where id = idShoes;
+    
+    if orderTemp = 0 or idOrders = null
+		then 
+			insert into orders(date, Customer_id) value (current_date(), idCustomer);
+            set orderTemp = last_insert_id();
+	end if;
+    
+    if  stockTemp > 0
+		then
+			insert into order_includes(Orders_id, Shoe_id) values(orderTemp, idShoes);
+            update shoe set shoe_stock = (shoe_stock)-1 where shoe.id = idShoes;
+	end if;
+END//
+delimiter ;
 
 
+create table Out_Of_Stock (
+id int not null auto_increment primary key, 
+date datetime not null DEFAULT CURRENT_TIMESTAMP,
+Shoe_id int not null, 
+foreign key (shoe_id) references shoe(id));
+
+-- drop trigger last_shoe;
+Delimiter //
+create trigger last_shoe
+after update
+on shoe
+for each row
+Begin
+	if new.shoe_stock = 0
+		then 
+			insert into out_of_stock (shoe_id) values (old.id);
+	end if;
+End//
+Delimiter ;
